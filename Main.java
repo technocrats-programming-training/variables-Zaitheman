82dde23bbe778ac1df286a75dccda6fd984e36a5
Zaithe <108100446+Zaitheman@users.noreply.github.com>
2022-06-30 20:42:33 +0000

    it no work but it's your code so you kinda dum dum get rekt.

--- Main.java
+++ Main.java
@@ -13,11 +13,27 @@ public class Main {
 
         // Print out the following string: "Hello, I am <firstName> <lastName>, and I am <age> years old"
 
+      firstName = "Geraldo";
+      lastName = "Nortono";
+      age = 14;
+      
+      String funkyTown = "Hello, I am " + firstName + " " + lastName + ", and I am " + age + " years old";
+        System.out.println(funkyTown);
+      
         // Print out whether age is even
-
+      
+      boolean evenOdd = age % 2 == 0;
+      System.out.println(evenOdd);
+      
         // Print out whether firstName equals lastName
-
+      
+      boolean sameName = (firstName == lastName);
+      System.out.println(sameName);
+      
         // Print out whether age is greater than 0
-
+      
+      boolean notZero = (age > 0);
+      System.out.println(notZero);
+      
     }
 }
